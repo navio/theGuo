98e79be225cb0e4ecf8bb573608e40021d7373ab
Alex Guo <alexguo@amazon.com>
2017-12-14 20:25:57 -0500

    add put

--- src/PostButton.js
+++ src/PostButton.js
@@ -8,6 +8,24 @@ class PostButton extends React.Component {
 
   postData() {
   	console.log(document.querySelector('.drpr').innerHTML);
+  	/*
+  	{"file-name":"blah", "htmlString":"anyValue"}
+  	PUT
+  	/upload
+  	                var url = "/ajax-refmarker" + ref;
+                 var request = new XMLHttpRequest();
+                 request.open('GET', url, true);
+                 request.send(url);
+  	*/
+  	let url = '/upload';
+  	let data = {
+  		'file-name': 'legit-test.html',
+  		'htmlString': document.querySelector('.drpr').innerHTML
+  	};
+  	let request = new XMLHttpRequest();
+  	request.open('PUT', url, true);
+  	request.setRequestHeader('Content-type','application/json; charset=utf-8');
+  	request.send(JSON.stringify(data));
   }
 
   render() {
